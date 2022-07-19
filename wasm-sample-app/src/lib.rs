98b7ec759e4e16d32007a6fc91611e2260772ec6
Peter Carr <peter.carr@threatx.com>
2022-07-19 10:33:33 -0700

    change return type of hello_wasm, add new imported function

--- wasm-sample-app/src/lib.rs
+++ wasm-sample-app/src/lib.rs
@@ -7,6 +7,7 @@ extern "C" {
     fn print_str(ptr: *const u8, len: usize);
     fn print_str2(ptr: *const u8, len: usize);
     fn increment_shared();
+    fn resolve_bool(ptr: *const u8, len: usize) -> i32;
 }
 
 // Define a string that is accessible within the wasm
@@ -16,7 +17,7 @@ static HELLO: &'static str = "Hello, World!";
 // Export a function named "hello_wasm". This can be called
 // from the embedder!
 #[no_mangle]
-pub extern "C" fn hello_wasm() {
+pub extern "C" fn hello_wasm() -> i32 {
     // Call the function we just imported and pass in
     // the offset of our string and its length as parameters.
     unsafe {
@@ -25,6 +26,7 @@ pub extern "C" fn hello_wasm() {
         increment_shared();
         increment_shared();
         print_str2(HELLO.as_ptr(), HELLO.len());
+        resolve_bool(HELLO.as_ptr(), HELLO.len())
     }
 }
 
